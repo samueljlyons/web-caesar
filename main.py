64646753ae083b659617561b1fe09b0624e6a3ae
Sam Lyons <lyonssamuelj@gmail.com>
2018-09-23 14:37:49 -0500

    finished web caesar

--- main.py
+++ main.py
@@ -1,42 +1,47 @@
-from flask import Flask
+from flask import Flask, request
 from caesar import rotate_string
-
-form="""
+app = Flask(__name__)
+app.config['DEBUG'] = True
+form = """ 
 <!DOCTYPE html>
-
 <html>
     <head>
         <style>
-            form{
-                background-color:#eee;
-                padding:20px;
-                margin:0 auto;
-                width:540 px;
+            form {{
+                background-color: #eee;
+                padding: 20px;
+                margin: 0 auto;
+                width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
-                width:540px;
-                height:120px;
-            }
+                width: 540px;
+                height: 120px;
+            }}
         </style>
     </head>
     <body>
-        <form method='post'>
-            <label for='rot'>Rotate by:</label>
-            <input id='rot' type='text' name='rot' value='0'>
-            <input name='text' type=textarea>
-            <input type='submit' value='Submit message'>
-        <!--create your form here-->
+        <form method='POST'>
+            <label>Rotate by:
+                <input name="rot" type="text" value="0" />    
+            </label>
+            <label>Text to be encoded:
+                <textarea name="text">{0}</textarea>
+            </label>
+            <input type ="submit" />
+        </form>
     </body>
 </html>
 """
-app=Flask(__name__)
-app.config['DEBUG']=True
-
+@app.route("/", methods=['POST'])
+def encrypt():
+    rot = int(request.form['rot'])
+    text = str(request.form['text'])
+    message = rotate_string(text, rot)
+    return "<h1>" + form.format(message) + "</h1>"
 @app.route("/")
 def index():
-    return form
-
+    return form.format('')
 app.run()
